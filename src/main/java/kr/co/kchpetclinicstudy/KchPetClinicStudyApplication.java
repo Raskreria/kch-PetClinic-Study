d218ede5e4c0843e1d7f39d3507c13e3c8b1c543
DESKTOP-TR1IQBS\sky <knkn9436@naver.com>
2023-03-18 16:42:10 +0900

    feat : init setting

---
+++ src/main/java/kr/co/kchpetclinicstudy/KchPetClinicStudyApplication.java
@@ -0,0 +1,16 @@
+package kr.co.kchpetclinicstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KchPetClinicStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KchPetClinicStudyApplication.class, args);
+//임시 처리
+    }
+
+}
+
+
---
+++ src/test/java/kr/co/kchpetclinicstudy/KchPetClinicStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kchpetclinicstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KchPetClinicStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
